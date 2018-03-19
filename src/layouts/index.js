742205151bd20d174f22d8d55038f3fdfd172a82
Andrew Reese <drewdrummer88@gmail.com>
2018-03-19 16:11:22 -0500

    changed up logos and title

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    < Helmet title = "Home | ΔNDRΞW" / >
     <Navbar />
     <div>{children()}</div>
   </div>
